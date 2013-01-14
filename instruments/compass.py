75fab6092690a503ee5991739510e48e0c9e9b33
Hans Maree <hans.maree@gmail.com>
2013-01-14 14:55:55 +0100

    Cleanup and documentation of the compass class.

--- instruments/compass.py
+++ instruments/compass.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import math
 from PyQt4 import QtGui,  QtCore
 
 
@@ -13,129 +14,128 @@ class SerialThread(QtCore.QThread):
 
     def run(self):
         try:
-            heading = 0
+            heading = 0.0
             for line in self.serial:
+                print(line)
                 l = line.strip().split()
-                if l[0] != "T:" or len(l) != 18:
+                if l[0] != "T:":
                     continue
-                if heading != float(l[10]):
-                    heading = float(l[10])
+                x = float(l[5]);
+                y = float(l[7]);
+                new_heading = math.atan2(y, x) / math.pi * 180
+
+                if heading != new_heading:
+                    heading = new_heading
                     print(heading)
                     self.headingChanged.emit(heading)
         except Exception as e:
             print(e)
+            pass
 
 
 class CompassWidget(QtGui.QWidget):
     
     def __init__(self,  heading=0):
         super(CompassWidget, self).__init__()
-        
         self.heading=heading
         
-        #Define Lines
-        self.thick=QtGui.QPen(QtCore.Qt.white,  6,  cap=QtCore.Qt.FlatCap)
-        self.medium=QtGui.QPen(QtCore.Qt.white,  4,  cap=QtCore.Qt.FlatCap)
-        self.thin=QtGui.QPen(QtCore.Qt.white,  2,  cap=QtCore.Qt.FlatCap)
-        self.plane=QtGui.QPen(QtCore.Qt.white, 2, cap=QtCore.Qt.FlatCap)
+        #Define Pens and fonts
+        self.thickPen=QtGui.QPen(QtCore.Qt.white,  6,  cap=QtCore.Qt.FlatCap)
+        self.mediumPen=QtGui.QPen(QtCore.Qt.white,  4,  cap=QtCore.Qt.FlatCap)
+        self.thinPen=QtGui.QPen(QtCore.Qt.white,  2,  cap=QtCore.Qt.FlatCap)
+        self.planePen=QtGui.QPen(QtCore.Qt.white, 2, cap=QtCore.Qt.FlatCap)
         
-        self.text2=QtGui.QPen(QtCore.Qt.yellow, 4,  cap=QtCore.Qt.FlatCap)
-        self.text=QtGui.QPen(QtCore.Qt.red, 4,  cap=QtCore.Qt.FlatCap)
-        self.textfont=QtGui.QFont("Times", 18, 75)
+        self.numbersPen=QtGui.QPen(QtCore.Qt.yellow, 4,  cap=QtCore.Qt.FlatCap)
+        self.textPen=QtGui.QPen(QtCore.Qt.red, 4,  cap=QtCore.Qt.FlatCap)
+        self.textFont=QtGui.QFont("Times", 18, 75)
         
-        planepoints=[[0, -100], [0, -85], [5, -75], [8, -40], [75, 5], [75, 20], [7, -5], [5, 40], [25, 60], [25, 70], [3,  58], [0, 62], [-3, 58], [-25, 70], [-25, 60], [-5, 40], [-7, -5], [-75, 20], [-75, 5], [-8, -40], [-5, -75], [0, -85]]
-        self.planepoly=self.poly(planepoints)
+        # Define points for the airplane shape
+        planepoints=[[0, -100], [0, -85], [5, -75], [8, -40], [75, 5], [75, 20], [7, -5], [5, 40], [25, 60], [25, 70], [3,  58], [0, 62]]
+        self.planePoly = QtGui.QPolygonF([ QtCore.QPointF(*p) for p in planepoints] + 
+                                         [ QtCore.QPointF(-p[0], p[1]) for p in reversed(planepoints[1:-1])])
         
-        self.initUI()
-        
-    def initUI(self):      
-
+        # Setup the user interface
         self.setGeometry(300, 300, 340, 340)
         self.setWindowTitle('Compass')
         self.show()
         
-    def poly(self, pts):
-        return QtGui.QPolygonF(list(map(lambda p: QtCore.QPointF(*p), pts)))
 
     def setHeading(self, heading):
+        """ Set the compass heading. """
         self.heading = heading
         self.update()
 
-    def paintEvent(self, e):
 
+    def paintEvent(self, e):
+        """ Draw the compass. """
         qp = QtGui.QPainter()
         qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing)
+        qp.setRenderHint(QtGui.QPainter.TextAntialiasing)
         qp.translate(170,  170)
         
-        color = QtGui.QColor(0, 0, 0)
-        color.setNamedColor('#d4d4d4')
-        qp.setPen(color)
+        # Draw the compass ring.
+        qp.save()
+        qp.rotate(self.heading)
+        self.drawRing(qp)
+        qp.restore()
+        
+        # Draw the airplane
+        qp.setPen(self.planePen)
+        qp.drawPolyline(self.planePoly)
+        
+        qp.end()
+        
+
+    def drawRing(self, qp):
+        """ Draw the moving ring of the compass."""
+        qp.setPen(QtGui.QColor(104, 104, 104))
         qp.setBrush(QtGui.QColor(0, 0, 0))
         qp.drawEllipse(-170, -170, 340, 340)
         
         qp.save()
 
-        qp.rotate(self.heading)
-        
-        rotation = 0
-        while rotation < 360:
-            
-            if rotation % 30 == 0:
-                qp.setPen(self.thick)
+        # draw the lines on the compass ring
+        for angle in range(0, 360, 5):
+            if angle % 30 == 0:
+                qp.setPen(self.thickPen)
                 qp.drawLine(0, -170, 0, -130)
-                
-                if rotation == 0:
-                    qp.setPen(self.text)
-                    qp.setFont(self.textfont)
-                    qp.drawText(-qp.fontMetrics().width("N")/2, -110,  "N")
-                elif rotation == 90:
-                    qp.setPen(self.text)
-                    qp.setFont(self.textfont)
-                    qp.drawText(-qp.fontMetrics().width("E")/2, -110,  "E")
-                elif rotation == 180:
-                    qp.setPen(self.text)
-                    qp.setFont(self.textfont)
-                    qp.drawText(-qp.fontMetrics().width("S")/2, -110,  "S")
-                elif rotation == 270:
-                    qp.setPen(self.text)
-                    qp.setFont(self.textfont)
-                    qp.drawText(-qp.fontMetrics().width("W")/2, -110,  "W")
-                else:
-                    qp.setPen(self.text2)
-                    qp.setFont(self.textfont)
-                    qp.drawText(-qp.fontMetrics().width(str(int(rotation/10)))/2, -110, str(int(rotation/10)))
-            elif rotation % 10 == 0:
-                qp.setPen(self.medium)
+            elif angle % 10 == 0:
+                qp.setPen(self.mediumPen)
                 qp.drawLine(0, -170, 0, -140)
-            elif rotation % 5 == 0:
-                qp.setPen(self.thin)
+            else:
+                qp.setPen(self.thinPen)
                 qp.drawLine(0, -170, 0, -150)
-            
             qp.rotate(5)
-            rotation += 5
 
         qp.restore()
-        
-        # Draw airplane
-        qp.setPen(self.plane)
-        qp.drawPolyline(self.planepoly)
-        
-        qp.end()
-        
-    def drawRectangles(self, qp):
-      
-        color = QtGui.QColor(0, 0, 0)
-        color.setNamedColor('#d4d4d4')
-        qp.setPen(color)
-        qp.setBrush(QtGui.QColor(0, 0, 0))
-        qp.drawEllipse(-45, -45, 90, 90)
-        
-        
-   
+        qp.save()
+
+        # Draw the compass headings
+        qp.setPen(self.textPen)
+        qp.setFont(self.textFont)
+        for text in ["N", "E", "S", "W"]:
+            qp.drawText(-qp.fontMetrics().width(text)/2, -110,  text)
+            qp.rotate(90)
+
+        qp.restore()
+        qp.save()
+
+        # draw the numbers
+        qp.setPen(self.numbersPen)
+        qp.setFont(self.textFont)
+        for angle in range(0, 360, 30):
+            if angle % 90 != 0:
+                text = str(int(angle/10))
+                qp.drawText(-qp.fontMetrics().width(text)/2, -110, text)
+            qp.rotate(30)
+            
+        qp.restore()
+
+
 def main():
-    
     app = QtGui.QApplication(sys.argv)
-    compass = CompassWidget()
+    compass = CompassWidget(15)
     thread = SerialThread()
     thread.headingChanged.connect(compass.setHeading)
     thread.start()

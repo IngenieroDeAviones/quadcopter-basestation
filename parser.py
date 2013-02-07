b247f5fa9cdc7c9e2364ff91731ad1dd88d65e89
Hans Maree <hans.maree@gmail.com>
2013-02-07 13:11:14 +0100

    Widget to display raw sensors data.

--- parser.py
+++ parser.py
@@ -9,51 +9,68 @@ class InvalidSyntaxError(Exception):
 
 
 class ParserThread(QtCore.QThread):
-    notification = QtCore.pyqtSignal(str, str)
-    error = QtCore.pyqtSignal(str, str)
-    sensorData = QtCore.pyqtSignal(list)
+    notification = QtCore.pyqtSignal(list, str)
+    error = QtCore.pyqtSignal(list, str)
+    sensorData = QtCore.pyqtSignal(list, list)
 
 
-    def __init__(self, stram):
-        super().__init()
+    def __init__(self, stream):
+        super().__init__()
         self.stream = stream
 
 
     def parseLine(self, line):
         try:
-            command, *arguments = line.split(' ')
+            command, *arguments = line.strip().split(' ')
         except:
             raise InvalidSyntaxError()
 
         cmd, *cmd_type = command
 
         if cmd == 'N':
-            self.notification.send(cmd_type, ' '.join(arguments))
+            self.notification.emit(cmd_type, ' '.join(arguments))
         elif cmd == 'E':
-            self.error.send(cmd_type, ' '.join(arguments))
+            self.error.emit(cmd_type, ' '.join(arguments))
         elif cmd == 'S':
-            self.sensorData.send(cmd_type, arguments)
+            self.sensorData.emit(cmd_type, arguments)
 
 
     
     def run(self):
-        data = ''
-        try:
-            data += self.stream.readall()
-            lines = data.split('\n')
-            for line in lines:
+        for line in self.stream:
+            try:
                 self.parseLine(line)
-            data = lines[-1]
-        except:
-            print(e)
-            pass
+            except Exception as e:
+                print(e)
 
 
-class SensorDataParser:
+class SensorDataParser(QtCore.QObject):
     gyroscopeData = QtCore.pyqtSignal(float, float, float)
     accelerometerData = QtCore.pyqtSignal(float, float, float)
     magnetometerData = QtCore.pyqtSignal(float, float, float)
     barrometerData = QtCore.pyqtSignal(float)
+    gpsData = QtCore.pyqtSignal(float, float, float, float)
+    currentData = QtCore.pyqtSignal(str, list)
+    temperatureData = QtCore.pyqtSignal(str, list)
 
     def __init__(self, parserThread):
-        
+        super().__init__()
+        parserThread.sensorData.connect(self.parseData)
+
+
+    def parseData(self, sensor, data):
+        if sensor[0] == 'G':
+            self.gyroscopeData.emit(*map(float, data[:3]))
+        elif sensor[0] == 'A':
+            self.accelerometerData.emit(*map(float, data[:3]))
+        elif sensor[0] == 'M':
+            self.magnetometerData.emit(*map(float, data[:3]))
+        elif sensor[0] == 'B':
+            self.barrometerData.emit(float(data[0]))
+        elif sensor[0] == 'P':
+            pass
+        elif sensor[0] == 'C':
+            pass
+        elif sensor[0] == 'T':
+            pass
+
---
+++ widgets/rawsensorwidget.py
@@ -0,0 +1,102 @@
+
+import sys
+from PyQt4 import QtCore, QtGui
+
+
+class RawSensorWidget(QtGui.QWidget):
+    def __init__(self, sensorDataParser):
+        super().__init__()
+        sensorDataParser.gyroscopeData.connect(self.gyroData)
+        sensorDataParser.accelerometerData.connect(self.accelData)
+        sensorDataParser.magnetometerData.connect(self.compassData)
+
+        self.gyroX = QtGui.QLabel()
+        self.gyroY = QtGui.QLabel()
+        self.gyroZ = QtGui.QLabel()
+        self.gyroX.setAlignment(QtCore.Qt.AlignRight)
+        self.gyroY.setAlignment(QtCore.Qt.AlignRight)
+        self.gyroZ.setAlignment(QtCore.Qt.AlignRight)
+
+        gyroBox = QtGui.QGroupBox('Gyroscope')
+        gyroBox.layout = QtGui.QGridLayout()
+        gyroBox.layout.addWidget(QtGui.QLabel('x:'), 0, 0)
+        gyroBox.layout.addWidget(QtGui.QLabel('y:'), 1, 0)
+        gyroBox.layout.addWidget(QtGui.QLabel('z:'), 2, 0)
+        gyroBox.layout.addWidget(self.gyroX, 0, 1)
+        gyroBox.layout.addWidget(self.gyroY, 1, 1)
+        gyroBox.layout.addWidget(self.gyroZ, 2, 1)
+        gyroBox.layout.addWidget(QtGui.QLabel('°/s'), 0, 2)
+        gyroBox.layout.addWidget(QtGui.QLabel('°/s'), 1, 2)
+        gyroBox.layout.addWidget(QtGui.QLabel('°/s'), 2, 2)
+        gyroBox.setLayout(gyroBox.layout)
+
+        self.accelX = QtGui.QLabel()
+        self.accelY = QtGui.QLabel()
+        self.accelZ = QtGui.QLabel()
+        self.accelX.setAlignment(QtCore.Qt.AlignRight)
+        self.accelY.setAlignment(QtCore.Qt.AlignRight)
+        self.accelZ.setAlignment(QtCore.Qt.AlignRight)
+
+        accelBox = QtGui.QGroupBox('Accelerometer')
+        accelBox.layout = QtGui.QGridLayout()
+        accelBox.layout.addWidget(QtGui.QLabel('x:'), 0, 0)
+        accelBox.layout.addWidget(QtGui.QLabel('y:'), 1, 0)
+        accelBox.layout.addWidget(QtGui.QLabel('z:'), 2, 0)
+        accelBox.layout.addWidget(self.accelX, 0, 1)
+        accelBox.layout.addWidget(self.accelY, 1, 1)
+        accelBox.layout.addWidget(self.accelZ, 2, 1)
+        accelBox.layout.addWidget(QtGui.QLabel('g'), 0, 2)
+        accelBox.layout.addWidget(QtGui.QLabel('g'), 1, 2)
+        accelBox.layout.addWidget(QtGui.QLabel('g'), 2, 2)
+        accelBox.setLayout(accelBox.layout)
+
+        self.compassX = QtGui.QLabel()
+        self.compassY = QtGui.QLabel()
+        self.compassZ = QtGui.QLabel()
+        self.compassX.setAlignment(QtCore.Qt.AlignRight)
+        self.compassY.setAlignment(QtCore.Qt.AlignRight)
+        self.compassZ.setAlignment(QtCore.Qt.AlignRight)
+
+        compassBox = QtGui.QGroupBox('Magnetometer')
+        compassBox.layout = QtGui.QGridLayout()
+        compassBox.layout.addWidget(QtGui.QLabel('x:'), 0, 0)
+        compassBox.layout.addWidget(QtGui.QLabel('y:'), 1, 0)
+        compassBox.layout.addWidget(QtGui.QLabel('z:'), 2, 0)
+        compassBox.layout.addWidget(self.compassX, 0, 1)
+        compassBox.layout.addWidget(self.compassY, 1, 1)
+        compassBox.layout.addWidget(self.compassZ, 2, 1)
+        compassBox.layout.addWidget(QtGui.QLabel('Gauss'), 0, 2)
+        compassBox.layout.addWidget(QtGui.QLabel('Gauss'), 1, 2)
+        compassBox.layout.addWidget(QtGui.QLabel('Gauss'), 2, 2)
+        compassBox.setLayout(compassBox.layout)
+
+        self.layout = QtGui.QVBoxLayout()
+        self.layout.addWidget(gyroBox)
+        self.layout.addWidget(accelBox)
+        self.layout.addWidget(compassBox)
+        self.setLayout(self.layout)
+
+
+    def gyroData(self, x, y, z):
+        self.gyroX.setText(str(x))
+        self.gyroY.setText(str(y))
+        self.gyroZ.setText(str(z))
+
+
+    def accelData(self, x, y, z):
+        self.accelX.setText(str(x))
+        self.accelY.setText(str(y))
+        self.accelZ.setText(str(z))
+
+
+    def compassData(self, x, y, z):
+        self.compassX.setText(str(x))
+        self.compassY.setText(str(y))
+        self.compassZ.setText(str(z))
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    sensors = RawSensorWidget(None)
+    sensors.show()
+    sys.exit(app.exec_())

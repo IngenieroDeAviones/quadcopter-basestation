7a3bc94f4a72f73b18a463e579ce72f75e375968
Luc Verhoeven <l.verhoeven@luxusweb.net>
2013-04-28 18:24:20 +0200

    Added mainwindow for instrument panel.

--- widgets/instruments/compass.py
+++ widgets/instruments/compass.py
@@ -27,7 +27,7 @@ class CompassWidget(QtGui.QWidget):
                                          [ QtCore.QPointF(-p[0], p[1]) for p in reversed(planepoints[1:-1])])
         
         # Setup the user interface
-        #self.setGeometry(300, 300, 340, 340)
+        self.setGeometry(300, 300, 340, 340)
         #self.setWindowTitle('Compass')
         #self.show()
         
